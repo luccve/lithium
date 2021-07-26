def5f899b58224ba04babca82b923833193f55b7
Estevão Lucasi <78768492+luccve@users.noreply.github.com>
2021-07-25 21:00:36 -0300

    Add files via upload
    V.410
    Implementação de arte no menu e sons.
    Alteração nos módulos

---
+++ lithium_v401/fase_22.py
@@ -0,0 +1,140 @@
+#combat aranha ou cobra
+
+from pygame import sprite
+from pygame.constants import KEYDOWN, K_ESCAPE, K_r, QUIT
+from pygame.display import list_modes
+import mov_fases
+import construir
+import imagem
+import pygame
+import funcoes
+from random import randint
+
+
+pygame.init()
+
+
+def fase_22():
+    #tela
+
+    screen = (1280,720)
+    JANELA = pygame.display.set_mode(screen,0,32)
+    pygame.display.set_caption('Lithium')
+    FPS = pygame.time.Clock()
+
+
+    #Constantes
+    x_init_player = 1100
+    y_init_player = 360
+    
+    pontos = 0 #CONTADOR DE MONSTROS QUE MORRERAM
+    speed = 120 #FPS
+    mov = 3 #VELOCIDADE DO EIXO Y DOS MONSTROS
+    
+    FLAG_TERMINOU_D2 = False
+
+    #Grupos
+    sprite_player = sprite.Group()
+    textos = sprite.Group()
+    bossAranha =  sprite.Group()
+    poisonAranha = sprite.Group()
+    spear_grupo = sprite.Group()
+
+        
+
+    #Objetos
+    player = construir.Player(x_init_player,y_init_player, 1, imagem.sprite_enya)
+    aranha =  construir.Aranha(245, 0,mov)
+    d2 = construir.Textos(640, 360, imagem.dialogo_2_img, 2)
+    poison = construir.Projetil(aranha, imagem.aranhaPoison, 18, 20, 4, 3)
+
+
+
+    #Adicionando 
+
+    sprite_player.add(player)
+    bossAranha.add(aranha)
+    poisonAranha.add(poison)
+    textos.add(d2)
+
+
+
+    while True:
+        #Desenhando
+        FPS.tick(speed)
+        JANELA.fill((0, 0, 0))
+
+        mov_fases.movfase22(spear_grupo,VELOCIDADE=50 ,player = player, FLAG_TEXTO=True, max_caixas=1)
+
+        n = mov_fases.n_enters()
+
+        d2.caixa(n)
+
+        textos.draw(JANELA)
+
+        if n > 0:
+            FLAG_TERMINOU_D2 = True
+            textos.remove(d2)
+
+
+        if FLAG_TERMINOU_D2: 
+
+            #Colisoes da aranha com o player
+            colisao_aranha = pygame.sprite.spritecollide(player, bossAranha, False, pygame.sprite.collide_mask)
+            colisao_poison = pygame.sprite.spritecollide(player, poisonAranha, False, pygame.sprite.collide_mask)
+
+            #Colisões do ataque do player com a aranha
+            colisao_fire1 = pygame.sprite.spritecollide(aranha, spear_grupo, True, pygame.sprite.collide_mask)
+
+            #Condições de vitória
+            if colisao_fire1:
+                pontos += 10
+                if pontos == 100:
+                    bossAranha.empty()
+                    poisonAranha.empty()
+
+            #Condições de Restart
+            if colisao_aranha:
+                funcoes.gameover(JANELA,poisonAranha,player)
+                pontos = 0
+                spear_grupo.empty()
+            if colisao_poison:
+                funcoes.gameover(JANELA,poisonAranha, player)
+                pontos = 0
+                spear_grupo.empty()
+            #Inteligência de ataque da Aranha
+            if pontos < 100:
+                if aranha.rect.centery == player.rect.centery or aranha.rect.centery == player.rect.centery+randint(1,100) or aranha.rect.centery == player.rect.centery - randint(1, 100):
+                    poison = construir.Projetil(aranha, imagem.aranhaPoison, 18, 20, 4, 3)
+                    poisonAranha.add(poison)
+                    
+            #Remover objetos para otimizar a memória
+            if poison.rect.x > 1280:
+                poisonAranha.remove(poison)
+            
+
+            sprite_player.draw(JANELA)
+            bossAranha.draw(JANELA)
+            spear_grupo.draw(JANELA)
+            poisonAranha.draw(JANELA)
+
+            if pontos <= 25 and pontos >0:
+                JANELA.blit(imagem.vida4,(500,50))
+                #funcoes.texto(f"{int(abs((pontos/100*100)-100))}%", 750, 50,tamanho=30)
+            if pontos <= 50 and pontos > 25:
+                JANELA.blit(imagem.vida3,(500,50))
+                
+            if pontos <= 75 and pontos > 50:
+                JANELA.blit(imagem.vida2,(500,50))
+                
+            if pontos <= 99 and pontos > 75:
+                JANELA.blit(imagem.vida1,(500,50))
+                
+            
+            sprite_player.update()
+            bossAranha.update()
+            poisonAranha.update()
+            spear_grupo.update()
+
+        pygame.display.flip() 
+
---
+++ lithium_v401/menu.py
@@ -0,0 +1,206 @@
+
+import pygame
+import imagem
+import som
+from pygame.locals import *
+from sys import exit
+from fase_1 import fase_1
+from fase_21 import fase_21
+from fase_22 import fase_22
+
+LARGURA = 1280
+ALTURA = 720
+FPS = 30
+
+selecao = 0
+intro = 0
+fase = 1
+
+#Music
+menusom = som.menu_som
+menusom2 = som.esc_som
+enter_menu = som.enter_som
+save = som.startfase_som
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((LARGURA, ALTURA), 0, 32)
+botao_enter = False
+
+
+def tela():
+    return screen.fill((0, 0, 0))
+
+def instrucoes():
+        
+    flag_instrucoes = True
+    while flag_instrucoes:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    flag_instrucoes = False
+              
+        screen.blit(imagem.instrucoes_img, (0, 0))
+        pygame.display.update()
+
+def fases():
+    global fase
+    
+    flag_fase = True
+    while flag_fase:
+        tela()
+        for event in pygame.event.get():
+            
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    fase=0
+                    flag_fase = False
+                if event.key == K_LEFT or event.key == K_a:
+                    fase-=1
+                    menusom2.play()
+                if event.key == K_RIGHT or event.key == K_d:
+                    fase+=1
+                    menusom2.play()
+                if event.key == K_RETURN:
+                    save.play()
+                    if fase == 1:
+                        fase_1()
+                    if fase == 2:
+                        fase_21()
+                    if fase == 3:
+                        fase_22()
+        
+        #Implementação da fase 
+        if fase > 3:
+            fase = 1
+        if fase < 1:
+            fase = 3   
+        if fase == 1:
+            tela()
+            screen.blit(imagem.stage1, (0, 0))
+
+        if fase == 2:
+            tela()
+            screen.blit(imagem.stage2, (0, 0))
+
+        if fase == 3:
+            tela()
+            screen.blit(imagem.stage3, (0, 0))
+        pygame.display.flip()
+
+def f_creditos():
+
+    creditos_menu = True
+    while creditos_menu:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    creditos_menu = False
+        screen.blit(imagem.creditos_img, (0, 0))
+        pygame.display.update()
+
+
+def events():
+    global selecao,intro,fase
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_s or event.key == K_DOWN:
+                menusom.play()
+                selecao += 1
+            if event.key == K_w or event.key == K_UP:
+                selecao -= 1
+                menusom.play()
+            if event.key == K_ESCAPE:
+                menusom.play()
+                pygame.quit()
+                exit()
+            if event.key == K_SPACE or event.key == K_RETURN:
+                intro +=1
+                enter_menu.play()
+                if selecao == 1:#Novo jogo
+                    fase_1()
+                if selecao == 2:#Creditos
+                    f_creditos()
+                if selecao == 3:#Instruções
+                    instrucoes()
+                if selecao == 4:#Fases
+                    fases()
+
+                if selecao == 5:#Configurações
+                    pass
+                    '''Em construção'''
+                if selecao == 6:#Sair
+                    pygame.quit()
+                    exit()
+
+def menu():
+    global selecao
+    if intro == 0:
+        
+        tela()
+        screen.blit(imagem.creditos_img, (0, 0))
+    else:
+        if selecao == 1:
+
+            tela()
+
+            screen.blit(imagem.menu1, (0, 0))
+
+        if selecao == 2:
+
+            tela()
+
+            screen.blit(imagem.menu2, (0, 0))
+
+        if selecao == 3:
+
+            tela()
+
+            screen.blit(imagem.menu3, (0, 0))
+
+        if selecao == 4:
+
+            tela()
+
+            screen.blit(imagem.menu4, (0, 0))
+
+        if selecao == 5:
+
+            tela()
+            screen.blit(imagem.menu5, (0, 0))
+
+        if selecao == 6:
+            tela()
+            screen.blit(imagem.menu6, (0, 0))
+
+
+        if selecao < 1:
+            selecao = 6
+
+        if selecao >= 7:
+            selecao = 1
+
+        
+
+while True:
+    tela()
+    
+    events()
+    menu()
+
+    pygame.display.flip()
